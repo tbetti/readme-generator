724c334054564a1ba7a67e4212267be861ba730a
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-08-04 08:50:45 -0400

    Added questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,54 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the project\'s title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What steps do you need to take to install this project?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license would you like to include on your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project?',
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who contributed to this project? List names and GitHub usernames with each person separated by a comma:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'List tests run on this application. Separate each with a comma:',
+    },
+    {
+        type: 'input',
+        name: 'gitHub',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
